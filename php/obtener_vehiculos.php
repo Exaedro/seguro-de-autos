b9c1df78a48db73ee818bfcf16a6d2eefad96214
Exaedro <77593869+Exaedro@users.noreply.github.com>
2023-09-29 19:09:34 -0300

    Add files via upload

---
+++ php/obtener_vehiculos.php
@@ -0,0 +1,24 @@
+<?php
+    $db = mysqli_connect('localhost', 'root', '', 'auto');
+    
+    $query = "SELECT v.tipo_vehiculo, v.modelo, v.marca, c.apellido FROM vehiculos v JOIN clientes c ON v.DNI_cliente = c.DNI_cliente;";
+    $result = mysqli_query($db, $query);
+
+    while($fila = mysqli_fetch_assoc($result)) {
+        $tipoVehiculo = $fila['tipo_vehiculo'];
+        $modelo = $fila['modelo'];
+        $marca = $fila['marca'];
+        $cliente = $fila['apellido'];
+
+        echo "<section class='auto'>";
+        echo "<h3>Tipo de Vehiculo</h3>";
+        echo "<p>$tipoVehiculo</p>";
+        echo "<h3>Marca</h3>";
+        echo "<p>$marca</p>";
+        echo "<h3>Modelo</h3>";
+        echo "<p>$modelo</p>";
+        echo "<h3>Cliente</h3>";
+        echo "<p>$cliente</p>";
+        echo "</section>";
+    }
+?>
---
+++ vehiculos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="#cobertura">Vehiculos</a></li>
+                    <li><a href="#precios">Precios</a></li>
+                    <li><a href="#contacto">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h1>Vehiculos de clientes</h1>
+        <?php include 'php/obtener_vehiculos.php'; ?>
+    </main>
+</body>
+</html>
